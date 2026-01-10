c5e297258d0efc5f11cd2afb716e6f73eb2d2856
RoockDev <zar0_31@outlook.com>
2026-01-10 18:09:57 +0100

    feat(controllers): implement createUser endpoint logic

---
+++ controllers/userController.js
@@ -0,0 +1,33 @@
+import { response, request } from 'express';
+import UserService from '../services/UserService.js';
+
+const userController = {
+    /**
+     * POST /api/users
+     */
+
+    createUser: async (req,res) => {
+        try{
+            const body = req.body;
+
+            const userService = new UserService();
+
+            const newUser = await userService.createUser(body);
+
+            return res.status(201).json({
+                success: true,
+                message: 'Usuario creado correctamente',
+                data: newUser
+            });
+        }catch(error){
+            console.log(error);
+            return res.status(500).json({
+                success:false,
+                message: 'Error al crear usuario',
+                data:null
+            });
+        }
+    }
+};
+
+export default userController;
